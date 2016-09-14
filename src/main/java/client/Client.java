499e1285834a9cf2f03622c2f29c32a506b245f4
Tima Tolochko <timur10645@icloud.com>
2016-09-14 21:21:50 +0300

    client program

--- src/main/java/client/Client.java
+++ src/main/java/client/Client.java
@@ -1,4 +1,100 @@
 package client;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
+/**
+ *Клиент
+ */
 public class Client {
+    private BufferedReader in;
+    private Socket socket;
+    private PrintWriter out;
+
+
+    // Запрашивает у юзера ник и организовывает обмен сообщениями с сервером
+    public Client() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Введите IP для подключения к серверу.");
+        System.out.println("Формат: xxx.xxx.xxx.xxx");
+
+        String ip = scanner.nextLine();
+
+        try {
+                //Коннектимся к серверу и подключаем потоки I/O для сообщений
+                socket = new Socket();
+                in     = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                out    = new PrintWriter(socket.getOutputStream(), true);
+
+                System.out.println("Введите свой ник");
+                out.println(scanner.nextLine());
+
+                // Запускаем мессенджер
+                Messenger messenger = new Messenger();
+                messenger.start();
+
+                // отправляет все что введет юзер на сервак
+                // пока не введет условие выхода "exit"
+                String str = "";
+                while (!str.equals("exit")){
+                        str = scanner.nextLine();
+                        out.println(str);
+                }
+                messenger.setStop();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            // Закрываем потоки и сокет
+                    try {
+                             in.close();
+                             out.close();
+                             socket.close();
+                    } catch (Exception ex) {
+                         ex.printStackTrace();
+                          }
+        }
+    }
+
+
+    /**
+     * Создает дополнительную нить для связи с консолью
+     * класс отправляет сообщения с сервера в консоль.
+     * setStop - выключатель
+     */
+    private class Messenger extends Thread{
+
+        private boolean stopped;
+
+        /**
+         * Прекращает пересылку сообщений
+         */
+        public void setStop(){
+            stopped = true;
+        }
+
+        /**
+         * Считывает все сообщения с сервера
+         *  и печатает их в консоль
+         */
+        @Override
+        public void run() {
+
+            try {
+                    while (!stopped){
+                        String str = in.readLine();
+                        System.out.println(str);
+                    }
+            } catch (IOException e){
+                System.out.println("message error");
+                e.printStackTrace();
+            }
+
+        }
+    }
 }
